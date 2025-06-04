de447928a74abbbf9ed5750d1928cb25df261159
skill <silenoksergey34@gmail.com>
2025-06-04 17:25:32 +0500

    added try/except checks. added explicit expectations

--- main_page/test_demo.py
+++ main_page/test_demo.py
@@ -1,9 +1,10 @@
-import time
-
 from selenium import webdriver
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 import pytest
 from faker import Faker
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture(scope="session")
@@ -12,7 +13,8 @@ def driver():
     browser.maximize_window()
     yield browser
 
-@pytest.fixture()
+
+@pytest.fixture
 def fake_auth_data():
     fake = Faker()
     return {
@@ -23,32 +25,64 @@ def fake_auth_data():
 
 def test_open_main_page(driver):
     driver.get("https://store.steampowered.com/")
-    popular_bar = driver.find_element(By.ID, 'home_featured_and_recommended')
-    popular_bar.is_enabled()
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.ID, 'home_featured_and_recommended')))
+    except TimeoutException:
+        pytest.fail("Элемент 'Популярное и рекомендуемое' не найден на главной странице")
 
 
 def test_open_login_page(driver):
-    burger_menu = driver.find_element(By.XPATH, '//*[@class="global_action_link"]')
-    burger_menu.is_enabled()
-    burger_menu.click()
-    time.sleep(2)
-    login_label = driver.find_element(By.XPATH, '//*[contains(@class, "ZrHHmwLEugLjLI")]')
-    login_label.is_displayed()
+    try:
+        login_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@class="global_action_link"]')))
+        login_button.click()
+    except TimeoutException:
+        pytest.fail("Кнопка 'войти' не найдена на главной странице")
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[contains(@class, "ZrHHmwLEugLjLI")]')))
+    except TimeoutException:
+        pytest.fail("Лейбл 'Вход' не найден на странице авторизации")
+
 
 def test_input_login_data(driver, fake_auth_data):
-    username_field = driver.find_element(By.XPATH, '(//*[contains(@class, "_2GBWeup5cttgbTw8FM3tfx")])[1]')
-    password_field = driver.find_element(By.XPATH,'(//*[contains(@class, "_2GBWeup5cttgbTw8FM3tfx")])[2]')
-    username_field.is_enabled()
-    password_field.is_enabled()
-    username_field.send_keys(fake_auth_data["username"])
-    password_field.send_keys(fake_auth_data["password"])
+    try:
+        username_field = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '(//*[contains(@class, "FM3tfx")])[1]')))
+        username_field.send_keys(fake_auth_data["username"])
+    except TimeoutException:
+        pytest.fail("Не найдено поле ввода имени пользователя")
+    try:
+        password_field = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '(//*[contains(@class, "FM3tfx")])[2]')))
+        password_field.send_keys(fake_auth_data["password"])
+    except TimeoutException:
+        pytest.fail("Не найдено поле ввода пароля")
+
 
 def test_show_preload_login_button(driver):
-    login_button = driver.find_element(By.XPATH, '//*[contains(@class, "DjSvCZoKKfoNSmarsEcTS")]')
-    login_button.click()
-    login_button_preloader = driver.find_element(By.XPATH,'//*[contains(@class, "_1VLukpV8qjL4BULw7Zob_l WYrJyNEVnjgAnMVZgvPeg")]')
-    login_button_preloader.is_displayed()
+    try:
+        auth_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[contains(@class, "DjSvCZo")]')))
+        auth_button.click()
+    except TimeoutException:
+        pytest.fail("Не найдена кнопка 'Войти'")
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[contains(@class,"WYrJyNEV")]')))
+    except TimeoutException:
+        pytest.fail("Не найден прелоадер у кнопки 'Войти'")
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.invisibility_of_element_located((By.XPATH, '//*[contains(@class,"WYrJyNEV")]')))
+    except TimeoutException:
+        pytest.fail("Не исчез прелоадер у кнопки 'Войти'")
+
 
 def test_show_auth_text_error(driver):
-    error_auth_text = driver.find_element(By.XPATH, '//*[contains(@class, "_1W_6HXiG4JJ0By1qN_0fGZ")]')
-    error_auth_text.is_displayed()
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[contains(@class, "N_0fGZ")]')))
+    except TimeoutException:
+        pytest.fail("Не найден текст ошибки под кнопкой 'Войти'")
