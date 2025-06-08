966d1074c0c627b79995568f273142337753244d
skill <silenoksergey34@gmail.com>
2025-06-08 13:14:39 +0500

    the comments on the review have been corrected

--- main_page/test_demo.py
+++ main_page/test_demo.py
@@ -18,50 +18,39 @@ AUTH_ERROR_TEXT = (By.XPATH, '//*[contains(@class, "tool-tip-source")]//followin
 fake = Faker()
 
 
-def wait_visibility_element(driver, locator, timeout=WAIT_TIMEOUT):
-    return WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator))
-
-
-def wait_invisibility_element(driver, locator, timeout=WAIT_TIMEOUT):
-    return WebDriverWait(driver, timeout).until_not(EC.visibility_of_element_located(locator))
-
-
-def wait_clickable_element(driver, locator, timeout=WAIT_TIMEOUT):
-    return WebDriverWait(driver, timeout).until(EC.element_to_be_clickable(locator))
-
-
 @pytest.mark.parametrize("username, password", [
     (fake.user_name(), fake.password()),
 ])
 def test_open_main_page(driver, username, password):
     driver.get(STORE_MAIN_PAGE)
 
-    assert wait_visibility_element(driver, locator=HOME_FEATURED_AND_RECOMMENDED), \
-        "Не найден элемент рекомендаций на главной странице"
+    WebDriverWait(driver, WAIT_TIMEOUT).until(
+        EC.visibility_of_element_located(HOME_FEATURED_AND_RECOMMENDED))
 
-    login_button = wait_clickable_element(driver, locator=LOGIN_BUTTON)
-    assert login_button is not None, "Не найдена кнопка 'Вход' на главной странице"
+    login_button = WebDriverWait(driver, WAIT_TIMEOUT).until(
+        EC.element_to_be_clickable(LOGIN_BUTTON))
     login_button.click()
 
-    assert wait_visibility_element(driver, locator=AUTH_FORM), "Не найдена форма авторизации"
+    WebDriverWait(driver, WAIT_TIMEOUT).until(
+        EC.visibility_of_element_located(AUTH_FORM))
 
-    username_field = wait_visibility_element(driver, locator=USERNAME_FIELD)
-    assert username_field is not None, "Не найдено поле ввода имени пользователя"
+    username_field = WebDriverWait(driver, WAIT_TIMEOUT).until(
+        EC.visibility_of_element_located(USERNAME_FIELD))
     username_field.send_keys(username)
 
-    password_field = wait_visibility_element(driver, locator=PASSWORD_FIELD)
-    assert password_field is not None, "Не найдено поле ввода пароля пользователя"
+    password_field = WebDriverWait(driver, WAIT_TIMEOUT).until(
+        EC.visibility_of_element_located(PASSWORD_FIELD))
     password_field.send_keys(password)
 
-    auth_button = wait_visibility_element(driver, locator=SUBMIT_BUTTON)
-    assert auth_button is not None, "Не найдена кнопка 'Вход' в форме авторизации"
+    auth_button = WebDriverWait(driver, WAIT_TIMEOUT).until(
+        EC.visibility_of_element_located(SUBMIT_BUTTON))
     auth_button.click()
 
-    assert wait_visibility_element(driver, locator=AUTH_BUTTON_PRELOADER), \
-        "Не найден прелоадер у кнопки 'Вход' в форме авторизации"
+    WebDriverWait(driver, WAIT_TIMEOUT).until(
+        EC.visibility_of_element_located(AUTH_BUTTON_PRELOADER))
 
-    assert wait_invisibility_element(driver, locator=AUTH_BUTTON_PRELOADER), \
-        "Прелоадер кнопки 'Вход' в форме авторизации не исчез"
+    WebDriverWait(driver, WAIT_TIMEOUT).until_not(
+        EC.visibility_of_element_located(AUTH_BUTTON_PRELOADER))
 
-    assert wait_visibility_element(driver, locator=AUTH_ERROR_TEXT), \
-        "Не найден текст ошибки при авторизации"
+    WebDriverWait(driver, WAIT_TIMEOUT).until(
+        EC.visibility_of_element_located(AUTH_ERROR_TEXT))
